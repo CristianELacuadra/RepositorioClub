86dc6035f745f2b5b712171d11eac14a68ae0692
MaxiGO <maximiliano-g@hotmail.com>
2016-10-21 19:11:43 -0300

    Borrado de GestorHermana

--- ProyectoClub/src/ar/com/ProyectoClub/Aplicacion/Gestores/GestorHermana.java
+++
@@ -1,9 +0,0 @@
-package ar.com.ProyectoClub.Aplicacion.Gestores;
-/**
- * 
- * @author Muro SE LA COME
- * @version 1.0
- */
-public class GestorHermana {
-	//Aca van las instrucciones para llevarlo al campito a pelu
-}
