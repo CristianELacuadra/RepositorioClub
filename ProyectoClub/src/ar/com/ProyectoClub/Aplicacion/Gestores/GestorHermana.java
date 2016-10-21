f30f425f143509c4c34e2785d0185295fd7ccd59
CristianELacuadra <cecl03@hotmail.com>
2016-10-21 19:08:20 -0300

    PeluLove

--- ProyectoClub/src/ar/com/ProyectoClub/Aplicacion/Gestores/GestorHermana.java
+++ ProyectoClub/src/ar/com/ProyectoClub/Aplicacion/Gestores/GestorHermana.java
@@ -2,8 +2,8 @@ package ar.com.ProyectoClub.Aplicacion.Gestores;
 /**
  * 
  * @author Muro SE LA COME
- *
+ * @version 1.0
  */
 public class GestorHermana {
-
+	//Aca van las instrucciones para llevarlo al campito a pelu
 }
